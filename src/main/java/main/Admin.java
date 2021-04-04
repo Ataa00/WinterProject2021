20289cbb79f86b3a9e9177f2fe1847492e78f7ad
omardere <omarderi80@gmail.com>
2021-04-04 17:49:38 +0300

    update

--- src/main/java/main/Admin.java
+++ src/main/java/main/Admin.java
@@ -1,9 +1,7 @@
 package main;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 public class Admin extends User {
-	private final static Logger LOGGER = 
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
 	public void addBook(String title,String author,String isbn,String signature)
 	{
 		int i;
@@ -11,7 +9,7 @@ public class Admin extends User {
 		{
 			if(((this.getLibrary().get(i)).getSignature()).equals(signature))
 			{
-		        LOGGER.log(Level.INFO, "the book is exist");
+		        System.out.println("this boook  exist");
 				i=-1;
 				break;
 			}
